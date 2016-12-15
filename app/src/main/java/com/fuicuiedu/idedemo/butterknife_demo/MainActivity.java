0c0554f5aabe2ac1ec83c73656dcfb9941f68893
wxcican <wxcican@qq.com>
2016-12-15 10:00:41 +0800

    Initial commit

---
+++ app/src/main/java/com/fuicuiedu/idedemo/butterknife_demo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.fuicuiedu.idedemo.butterknife_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_tv)
+    TextView mTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mTv.setText("呵呵");
+
+    }
+}
