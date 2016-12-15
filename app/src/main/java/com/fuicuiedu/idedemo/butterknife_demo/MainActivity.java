5943546a6e2045feb0ae07cb9de86afeaccddfc6
wxcican <wxcican@qq.com>
2016-12-15 11:10:16 +0800

    butterknife完成

--- app/src/main/java/com/fuicuiedu/idedemo/butterknife_demo/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/idedemo/butterknife_demo/MainActivity.java
@@ -1,25 +1,57 @@
 package com.fuicuiedu.idedemo.butterknife_demo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.List;
+
+import butterknife.BindString;
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.main_tv)
-    TextView mTv;
+    @BindViews({R.id.main_tv1,R.id.main_tv2,R.id.main_tv3})
+    List<TextView> textViewList;
+
+    private Unbinder unBinder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        //必须调用的方法！
+        unBinder = ButterKnife.bind(this);
+    }
 
-        mTv.setText("呵呵");
+    @OnClick({R.id.main_btn1,R.id.main_btn2,R.id.main_btn3})
+    public void aaa(View v){
+        switch (v.getId()){
+            case R.id.main_btn1:
+                Toast.makeText(this, "点击了Button1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.main_btn2:
+                Toast.makeText(this, "点击了Button2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.main_btn3:
+                Toast.makeText(this, "点击了Button3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在生命周期结束时解绑
+        unBinder.unbind();
     }
 }
